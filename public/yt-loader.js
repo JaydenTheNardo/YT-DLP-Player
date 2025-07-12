296165a49637b1fe6d161690e2b126b9076e148d
Jayden <jayden@jaydenha.uk>
2025-07-13 02:14:44 +0800

    fetch both streams in parallel

--- public/yt-loader.js
+++ public/yt-loader.js
@@ -107,8 +107,12 @@ async function mergeVideo(video, audio) {
     let { createFFmpeg } = FFmpeg;
     let ffmpeg = createFFmpeg();
     await ffmpeg.load();
-    ffmpeg.FS('writeFile', 'video.mp4', await fetchWithProgress(video, videoProgress));
-    ffmpeg.FS('writeFile', 'audio.mp4', await fetchWithProgress(audio, audioProgress));
+    const [videoData, audioData] = await Promise.all([
+        fetchWithProgress(video, videoProgress),
+        fetchWithProgress(audio, audioProgress)
+    ]);
+    ffmpeg.FS('writeFile', 'video.mp4', videoData);
+    ffmpeg.FS('writeFile', 'audio.mp4', audioData);
     await ffmpeg.run('-i', 'video.mp4', '-i', 'audio.mp4', '-c', 'copy', 'output.mp4');
     let data = await ffmpeg.FS('readFile', 'output.mp4');
     return new Uint8Array(data.buffer);
